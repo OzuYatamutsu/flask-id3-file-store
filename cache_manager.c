6bf60e3eb9ebd61a84b2e6bee399951cec74d0ac
Robert <rhensey@gatech.edu>
2015-11-17 18:59:47 -0500

    made spaces tabs

--- cache_manager.c
+++ cache_manager.c
@@ -8,15 +8,15 @@ static meta_cache_entry meta_cache[MAX_META_ENTRIES];
 static void getFileInCache(char* path)
 {
 	//avoid compiler warnings
-    path = path + 1;
+	path = path + 1;
 }
 
 //Give path to cached file
 char* getCachePath(char* path)
 {
 	//avoid compiler warnings
-    meta_cache[0].fileCacheIndex = 0;
-    path = path + 1;
-    getFileInCache(NULL);
-    return "/home/rhensey/TheSearch.mp3";
+	meta_cache[0].fileCacheIndex = 0;
+	path = path + 1;
+	getFileInCache(NULL);
+	return "/home/rhensey/TheSearch.mp3";
 }

c642b46823ddc65cc0b9953900fbd43890296341
Sean Collins <scollins36@gatech.edu>
2015-11-17 22:48:37 +0000

    Simple POST file upload

--- server.py
+++ server.py
@@ -1,5 +1,12 @@
-from flask import Flask
+from flask import Flask, request
+from werkzeug import secure_filename
+from os import path
+
+PORT = 9880 # Debug
+DATA_DIR = "./data"
+
 app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = DATA_DIR
 
 @app.route("/")
 def ytfs_hello():
@@ -7,8 +14,21 @@ def ytfs_hello():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    pass
+    """Uploads a file to the YTFS data store.
+    POST to /upload with a file_data field."""
+    file = request.files["file_data"]
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(path.join(app.config["UPLOAD_FOLDER"], filename))
+        return "Got data! Filename=" + filename # Debug
+    return "Upload failure!"
 
 @app.route("/get_file", methods=["GET"])
 def get_file():
     pass
+
+if __name__ == "__main__":
+    app.run(
+        host = "0.0.0.0",
+        port = PORT
+    )

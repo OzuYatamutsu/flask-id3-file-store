333e042642c8ace1a0fb75679134aa3b2b918aec
Sean Collins <scollins36@gatech.edu>
2015-11-18 16:12:58 -0500

    Fix POST

--- server/example-post.py
+++ server/example-post.py
@@ -4,7 +4,7 @@ from pprint import pprint
 
 if len(argv) == 2:
     pprint(
-        post("http://localhost:9880/upload", file_data={open(argv[1], "rb")})
+        post("http://localhost:9880/upload", files={"file_data": open(argv[1], "rb")})
     )
 else:
     print("Usage: python3 example-post.py <path_to_mp3_file>")

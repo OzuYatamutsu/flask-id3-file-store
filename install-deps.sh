825947d90227c7b4ae5f706519bc9701f42ac066
Sean Collins <scollins36@gatech.edu>
2015-11-27 06:00:51 +0000

    Fix python3 pip problem

--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt-get install python3 libfuse-dev pkg-config mp3info curl jq -y
+sudo apt-get install python3 python3-pip libfuse-dev pkg-config curl -y
 sudo pip3 install stagger
